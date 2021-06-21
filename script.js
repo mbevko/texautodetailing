cca3caff4f0ee7c5a2be8e9b6ba8456053d4aefe
Michael Evko <mbevko@gmail.com>
2021-06-21 18:31:22 -0400

    finished desktop guages

---
+++ script.js
@@ -0,0 +1,35 @@
+let filler = document.querySelectorAll(".bar .filler")
+let pbar = document.querySelectorAll(".full_gauge .pbar");
+const speed = 50;
+
+
+
+
+window.addEventListener('scroll', () => {
+  if(scrollY > 935){
+  filler.forEach(fill => {
+    fill.style.cssText = `width: inherit; background: ${fill.getAttribute('data-bg')}`
+  });
+    
+    pbar.forEach(counter => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    
+    const inc = target / speed;
+    
+   if(count < target) {
+      counter.innerText = Math.floor(count + inc);
+      setTimeout(updateCount, 60)
+      
+    } else {
+      count.innerText = target;
+    }
+  }
+ 
+  updateCount()
+})
+
+}
+})
+
