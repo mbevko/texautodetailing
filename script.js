759a49afd80fa0f5731d2cfeee61b422ce3e1e7f
Michael Evko <mbevko@gmail.com>
2021-06-21 19:29:02 -0400

    updating mobile, fixing bugs

--- script.js
+++ script.js
@@ -6,30 +6,29 @@ const speed = 50;
 
 
 window.addEventListener('scroll', () => {
-  if(scrollY > 935){
-  filler.forEach(fill => {
-    fill.style.cssText = `width: inherit; background: ${fill.getAttribute('data-bg')}`
-  });
-    
-    pbar.forEach(counter => {
-  const updateCount = () => {
-    const target = +counter.getAttribute("data-target");
-    const count = +counter.innerText;
-    
-    const inc = target / speed;
-    
-   if(count < target) {
-      counter.innerText = Math.floor(count + inc);
-      setTimeout(updateCount, 60)
-      
-    } else {
-      count.innerText = target;
+    if (scrollY > 935) {
+        filler.forEach(fill => {
+            fill.style.cssText = `width: inherit; background: ${fill.getAttribute('data-bg')}`
+        });
+
+        pbar.forEach(counter => {
+            const updateCount = () => {
+                const target = +counter.getAttribute("data-target");
+                const count = +counter.innerText;
+
+                const inc = target / speed;
+
+                if (count < target) {
+                    counter.innerText = Math.floor(count + inc);
+                    setTimeout(updateCount, 60)
+
+                } else {
+                    count.innerText = target;
+                }
+            }
+
+            updateCount()
+        })
+
     }
-  }
- 
-  updateCount()
-})
-
-}
-})
-
+});
