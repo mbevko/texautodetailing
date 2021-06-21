96cbee50a4d93ba6da5144f8f435cb0577f286bc
Michael Evko <mbevko@gmail.com>
2021-06-21 19:47:00 -0400

    updated mobile

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const speed = 50;
 
 
 window.addEventListener('scroll', () => {
-    if (scrollY > 935) {
+    if (scrollY > 935 && screen.width > 800) {
         filler.forEach(fill => {
             fill.style.cssText = `width: inherit; background: ${fill.getAttribute('data-bg')}`
         });
@@ -30,5 +30,28 @@ window.addEventListener('scroll', () => {
             updateCount()
         })
 
+    } else if(scrollY > 540 && screen.width < 800){
+        filler.forEach(fill => {
+            fill.style.cssText = `width: inherit; background: ${fill.getAttribute('data-bg')}`
+        });
+
+        pbar.forEach(counter => {
+            const updateCount = () => {
+                const target = +counter.getAttribute("data-target");
+                const count = +counter.innerText;
+
+                const inc = target / speed;
+
+                if (count < target) {
+                    counter.innerText = Math.floor(count + inc);
+                    setTimeout(updateCount, 60)
+
+                } else {
+                    count.innerText = target;
+                }
+            }
+
+            updateCount()
+        })
     }
 });
